b7ef79734449668067563c373b070ecb3b482f43
Koshal Parwan <nileshparwan.utm.bse16b@gmail.com>
2023-08-22 12:47:30 +0400

    [lesson1] type and data

--- graphqlnodejs/app.js
+++ graphqlnodejs/app.js
@@ -6,6 +6,8 @@ const { buildSchema } = require('graphql');
 // variable
 const app = express();
 
+const events = [];
+
 // middlewares
 app.use(bodyParser.json());
 
@@ -17,12 +19,27 @@ app.use(
     '/graphql',
     graphqlHTTP({
         schema: buildSchema(`
+            type Event {
+                _id: ID!
+                title: String!,
+                description: String!
+                price: Float!
+                date: String!
+            }
+
+            input EventInput {
+                title: String!
+                description: String!
+                price: Float!
+                date: String!
+            }
+
             type RootQuery {
-                events: [String!]!
+                events: [Event!]!
             }
 
             type RootMutation {
-                createEvent(name: String): String
+                createEvent(eventInput: EventInput): Event
             }
 
             schema {
@@ -33,19 +50,26 @@ app.use(
         rootValue: {
             // all resolvers function in it. and resolvers function must reach out schemas endpoint by name
             events: () => {
-                return ["romantic", "cooking", "sailing", "coding"];
+                return events;
             },
             createEvent: (args) => {
-                const eventName = args.name;
-                return eventName;
+                const event = {
+                    _id: Math.random().toString(),
+                    title: args.eventInput.title,
+                    description: args.eventInput.description,
+                    price: +args.eventInput.price,
+                    date: args.eventInput.date
+                };
+                events.push(event);
+                return event; 
             }
         },
         graphiql: true
-    })
+    }) 
 );
 
 
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
-  console.log(`Server is running on port ${PORT}`);
+    console.log(`Server is running on port ${PORT}`);
 });
